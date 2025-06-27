990117d2a9e3c023a33b41232622bfed9c01f043
Pourik Saha <pouriksaha2001@gmail.com>
2025-06-27 15:51:38 +0530

    Intercepting API Response

--- tests/apiDemo.spec.ts
+++ tests/apiDemo.spec.ts
@@ -64,3 +64,45 @@ test("Fetching Token from Header Demo", async ({ page, request }) => {
   await page.getByText("Delete Article").first().click();
   await page.getByText("Global Feed").click();
 })
+
+test("Deleting Article using DELETE Request", async ({ page, request }) => {
+  await page.getByText("New Article").click();
+  await page.getByPlaceholder("Article Title").fill("Test Article");
+  await page.getByPlaceholder("What's this article about?").fill("Testing Delete Request");
+  await page.getByPlaceholder("Write your Article (in markdown)").fill("Test Article Deletion using DELETE Request");
+  await page.getByRole("button", { name: "Publish Article" }).click();
+  
+  const articleResponse = await page.waitForResponse(
+    "https://conduit-api.bondaracademy.com/api/articles/"
+  );
+  const articleResponseBody = await articleResponse.json();
+  const slugId = articleResponseBody.article.slug;
+  console.log(slugId);
+
+  await page.getByText("Home").click();
+  await page.getByText("Global Feed").click();
+
+  const response = await request.post("https://conduit-api.bondaracademy.com/api/users/login", {
+    data: {
+      "user":{"email":"Pourik@conduit.com","password":"Pourik@conduit"}
+    }
+  })
+  const responseBody = await response.json();
+  const accessToken = responseBody.user.token;
+
+  const deleteArticleRequest = await request.delete(`https://conduit-api.bondaracademy.com/api/articles/${slugId}`, {
+      headers: {
+        Authorization: `Token ${accessToken}`
+      }
+    }
+  );
+  
+  expect(deleteArticleRequest.status()).toEqual(204);
+
+  await page.reload();
+
+  await expect(page.locator("app-article-list .article-preview h1").first()).not.toContainText(
+    "Test Article"
+  );
+
+});
